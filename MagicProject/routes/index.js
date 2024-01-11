3678dee760ef8fca5e71ed519161e30760095dc7
TouchMe <pro319_pro319@mail.ru>
2024-01-11 18:54:03 +0300

    Сделал функциональный LogOut

--- MagicProject/routes/index.js
+++ MagicProject/routes/index.js
@@ -46,4 +46,10 @@ router.post('/logreg', async function(req, res, next) {
   }
 });
   
+router.post('/logout', function(req, res, next) {
+  req.session.destroy()
+  res.locals.user = null
+  res.redirect('/')
+});
+
 module.exports = router;
