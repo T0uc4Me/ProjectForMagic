c6481e814f3247ca6570fe8bb1468131cfdf2fc9
TouchMe <pro319_pro319@mail.ru>
2024-01-11 17:42:23 +0300

    Сделал глобальную переменную для навигации

--- MagicProject/app.js
+++ MagicProject/app.js
@@ -38,11 +38,11 @@ app.use(function(req,res,next){
   next()
 })
 
-
+app.use(require("./middleware/createMenu.js"))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/elems', elems);
-app.use(require("./middleware/createMenu.js"))
+
   
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
