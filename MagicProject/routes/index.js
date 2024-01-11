aaa39a333541d5e86e27977f117ca2bdca36bcba
TouchMe <pro319_pro319@mail.ru>
2024-01-11 20:06:25 +0300

    Исправил баг, когда нельзя было выйти с аккаунта

--- MagicProject/routes/index.js
+++ MagicProject/routes/index.js
@@ -22,32 +22,35 @@ router.get('/logreg', async function(req, res, next) {
 router.post('/logreg', function(req, res, next) {
   var username = req.body.username
   var password = req.body.password
-  db.query (`SELECT * FROM user WHERE user.username = '${req.body.username}'`,
-  function(err,users){
-  if(err) return next(err)
-  if(users.length > 0) {
-  var user = users[0];
-  if (password == user.password){
-  req.session.user = user.id
-  res.redirect('/')
-  } else {
-  res.render('logreg', {title: 'Вход'})
-  }
-  } else {
-  db.query(`INSERT INTO user (username, password) VALUES ('${username}',
-  '${password}')`, function(err, user){
-  if(err) return next(err)
-  req.session.user = user.id
-  res.redirect('/')
+
+  db.query(`SELECT * FROM user WHERE user.username = '${req.body.username}'`, function(err, users) {
+      if (err) return next(err)
+      if (users.length > 0) {
+          var user = users[0];
+          if (password == user.password) {
+              req.session.user = user.id
+              res.redirect('/')
+          } else {
+              res.render('logreg', { title: 'Вход' })
+          }
+      } else {
+          db.query(`INSERT INTO user (username, password) VALUES ('${username}', '${password}')`, function(err, user) {
+              if (err) return next(err)
+              req.session.user = user.id
+              res.redirect('/')
+          })
+      }
   })
-  }
-  })
-  });
-  
+});
+
+// Маршрут для выхода
 router.post('/logout', function(req, res, next) {
-  req.session.destroy()
-  res.locals.user = null
-  res.redirect('/')
+  req.session.destroy(function(err) {
+      if (err) {
+          return next(err);
+      }else
+        res.redirect('/');
+  });
 });
 
 module.exports = router;
