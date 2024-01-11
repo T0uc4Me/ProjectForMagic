5f35e65ac40ca3b5cda05d9a12f1572d26edabf4
TouchMe <pro319_pro319@mail.ru>
2024-01-11 20:00:34 +0300

    Сделана информация о пользователе

--- MagicProject/middleware/createUser.js
+++ MagicProject/middleware/createUser.js
@@ -1,16 +1,15 @@
-// const User = require("./../models/user").User;
-
-// module.exports = async (req, res, next) => {
-//     try {
-//         // res.locals.user = null;
-//         // const user = await User.findById(req.session.user);
-        
-//         // if (user) {
-//         //     res.locals.user = user;
-//         // }
-
-//         next();
-//     } catch (err) {
-//         next(err);
-//     }
-// };
+var db = require('../mySQLConnect.js');
+module.exports = function(req,res,next) {
+res.locals.user = null
+console.log('Проверяем сессию')
+console.log(req.session.user)
+db.query(`SELECT * FROM user WHERE user.id = '${req.session.user}'`, (err,
+users) => {
+if(err)
+return next(err)
+if(users.length > 0) {
+res.locals.user = users[0];
+}
+next()
+})
+}
--- MagicProject/routes/index.js
+++ MagicProject/routes/index.js
@@ -1,18 +1,15 @@
-var express = require('express');
-var router = express.Router();
-
-//var User = require("./../models/user").User
+const express = require('express');
+const router = express.Router();
+//var User = require("../models/user").User
 var db = require('../mySQLConnect.js');
+
+
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   try {
-    
     req.session.greeting = "Hi!!!"
-    res.render('index', 
-    { title: 'Express', 
-      counter:req.session.counter 
-    });
+    res.render('index', { title: 'Express', counter:req.session.counter, error:null });
   } catch (err) {
     next(err);
   }
@@ -27,24 +24,25 @@ router.post('/logreg', function(req, res, next) {
   var password = req.body.password
   db.query (`SELECT * FROM user WHERE user.username = '${req.body.username}'`,
   function(err,users){
-    if(err) return next(err)
-    if(users.length > 0) {
-        var user = users[0];
-      if (password == user.password){
-        req.session.user = user.id
-        res.redirect('/')
-      } else {
-        res.render('logreg', {title: 'Вход'})
-      }
-    } else {
-      db.query(`INSERT INTO user (username, password) VALUES ('${username}','${password}')`, function(err, user){
-    if(err) return next(err)
-    req.session.user = user.id
-    res.redirect('/')
+  if(err) return next(err)
+  if(users.length > 0) {
+  var user = users[0];
+  if (password == user.password){
+  req.session.user = user.id
+  res.redirect('/')
+  } else {
+  res.render('logreg', {title: 'Вход'})
+  }
+  } else {
+  db.query(`INSERT INTO user (username, password) VALUES ('${username}',
+  '${password}')`, function(err, user){
+  if(err) return next(err)
+  req.session.user = user.id
+  res.redirect('/')
   })
-}
-})
-});
+  }
+  })
+  });
   
 router.post('/logout', function(req, res, next) {
   req.session.destroy()
