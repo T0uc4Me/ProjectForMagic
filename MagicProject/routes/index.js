9c71635f9b66c055c604f0a1e21fe8b5c908f353
TouchMe <pro319_pro319@mail.ru>
2024-01-11 16:41:47 +0300

    Теперь сессии работают корректно

--- MagicProject/routes/index.js
+++ MagicProject/routes/index.js
@@ -3,13 +3,16 @@ var router = express.Router();
 var Elem = require("../models/magic").Elem
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  Elem.find({},{_id:0,title:1,nick:1},function(err,menu){
+router.get('/', async (req, res, next) => {
+  try {
+    const menu = await Elem.find({}, { _id: 0, title: 1, nick: 1 });
     res.render('index', {
-    title: 'Express',
-    menu: menu
+      title: 'Express',
+      menu: menu
     });
-    })
+  } catch (err) {
+    next(err);
+  }
 });
 
 
