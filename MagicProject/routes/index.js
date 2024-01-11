978a7bb5a3536df300ffe66a541aad34608214f3
TouchMe <pro319_pro319@mail.ru>
2024-01-11 18:20:51 +0300

    Сделал страницу регистрации

--- MagicProject/routes/index.js
+++ MagicProject/routes/index.js
@@ -15,5 +15,8 @@ router.get('/', async (req, res, next) => {
     next(err);
   }
 });
-
+router.get('/logreg', function(req, res, next) {
+  res.render('logreg',{title: 'Вход'});
+  });
+  
 module.exports = router;
