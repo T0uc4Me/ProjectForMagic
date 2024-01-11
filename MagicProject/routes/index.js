40116c929ef4aa3a3c5619ce7d6591f5e56c443e
TouchMe <pro319_pro319@mail.ru>
2024-01-11 16:10:47 +0300

    Сделал команду записи cookie

--- MagicProject/routes/index.js
+++ MagicProject/routes/index.js
@@ -5,7 +5,7 @@ var Elem = require("../models/magic").Elem
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Elem.find({},{_id:0,title:1,nick:1},function(err,menu){
-    res.render('index', {
+    res.cookie('Приветствие', 'Здравствуй').res.render('index', {
     title: 'Express',
     menu: menu
     });
