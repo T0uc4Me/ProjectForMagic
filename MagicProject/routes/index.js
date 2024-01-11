79e5e7cb0e6891b337ad84be984db4e6a61ef151
TouchMe <pro319_pro319@mail.ru>
2024-01-11 18:35:45 +0300

    Написал логику пользователя

--- MagicProject/routes/index.js
+++ MagicProject/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Elem = require("../models/magic").Elem
+var User = require("./../models/user").User
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
@@ -15,14 +16,34 @@ router.get('/', async (req, res, next) => {
     next(err);
   }
 });
+
 router.get('/logreg', function(req, res, next) {
   res.render('logreg',{title: 'Вход'});
-  });
+});
 
-router.post('/logreg', function(req, res, next) {
-  var username = req.body.username
-  var password = req.body.password
+router.post('/logreg', async (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
+  try {
+      const user = await User.findOne({ username: username });
+
+      if (user) {
+          if (user.checkPassword(password)) {
+              req.session.user = user._id;
+              res.redirect('/');
+          } else {
+              res.render('logreg', { title: 'Вход' });
+          }
+      } else {
+          const newUser = new User({ username: username, password: password });
+          const savedUser = await newUser.save();
+          req.session.user = savedUser._id;
+          res.redirect('/');
+      }
+  } catch (err) {
+      next(err);
+  }
 });
   
 module.exports = router;
