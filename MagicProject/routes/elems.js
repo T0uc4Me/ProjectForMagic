d5396aeda09def240a24259ce9bf04d29e2cc997
TouchMe <pro319_pro319@mail.ru>
2024-01-11 19:30:25 +0300

    Сделал вывод данных на экран

--- MagicProject/routes/elems.js
+++ MagicProject/routes/elems.js
@@ -1,36 +1,55 @@
 var express = require('express');
 var router = express.Router();
-var Elem = require("../models/magic").Elem
+//var Elem = require("../models/magic").Elem
 var async = require("async")
-var checkAuth = require("./../middleware/checkAuth.js")
+var db = require('../mySQLConnect.js');
+//var checkAuth = require("./../middleware/checkAuth.js")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('Маршрутизатор 3-х стихий');
 });
 
-router.get("/:nick", checkAuth, async (req, res, next) => {
-    try {
-        const [elem, elems] = await Promise.all([
-            Elem.findOne({ nick: req.params.nick }),
-            Elem.find({}, { _id: 0, title: 1, nick: 1 })
-          ]);
+router.get("/:nick", function(req, res, next) {
+  db.query(`SELECT * FROM elements WHERE elements.nick = '${req.params.nick}'`, (err,
+  elems) => {
+  if(err) {
+  console.log(err);
+  if(err) return next(err)
+  } else {
+  if(elems.length == 0) return next(new Error("Стихия испарилась..."))
+  var elem = elems[0];
+  res.render('magic', {
+  title: elem.title,
+  picture: elem.avatar,
+  desc: elem.about
+})
+}
+})
+});
 
-      if (!elem) {
-        throw new Error("Стихия испарилась");
-      }
-      renderMagic(res, elem.title, elem.avatar, elem.desc, elems);
-    } catch (err) {
-      next(err);
-    }
-  });
+// router.get("/:nick", async (req, res, next) => {
+//     try {
+//         const [elem, elems] = await Promise.all([
+//             Elem.findOne({ nick: req.params.nick }),
+//             Elem.find({}, { _id: 0, title: 1, nick: 1 })
+//           ]);
 
-  function renderMagic(res, title, picture, desc, elems) {
-    res.render('magic', {
-        title: title,
-        picture: picture,
-        desc: desc
-    });
-  }
+//       if (!elem) {
+//         throw new Error("Стихия испарилась");
+//       }
+//       renderMagic(res, elem.title, elem.avatar, elem.desc, elems);
+//     } catch (err) {
+//       next(err);
+//     }
+//   });
+
+//   function renderMagic(res, title, picture, desc, elems) {
+//     res.render('magic', {
+//         title: title,
+//         picture: picture,
+//         desc: desc
+//     });
+//   }
 
 module.exports = router;
