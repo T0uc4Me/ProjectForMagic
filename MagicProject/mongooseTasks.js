5fe9e7a01d75c8dfba1dbf61a3c6052cba5d7487
TouchMe <pro319_pro319@mail.ru>
2024-01-11 15:12:43 +0300

    Сделал схему

--- MagicProject/mongooseTasks.js
+++ MagicProject/mongooseTasks.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1/test');
 
-const Magic = mongoose.model('Magic', { name: String });
+var schema = mongoose.Schema({ name: String })
+schema.methods.noise = function(){
+    console.log(this.get("name") + " издал все звуки стихий")
+}
+const Magic = mongoose.model('Magic', schema);
+
 const element = new Magic({ name: 'ВодОньЕтер' });
 
 element.save()
     .then(() => {
-        console.log('Шум ветра моря и огня, ласкают ваши уши...');
+        element.noise();
     })
     .catch((err) => {
         console.error(err);
