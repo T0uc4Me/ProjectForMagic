333d10b4e9c063fa0adf7ca7321a565453d8e63d
TouchMe <pro319_pro319@mail.ru>
2024-01-11 16:03:46 +0300

    Сделал заполнение меню из базы данных

--- MagicProject/routes/elems.js
+++ MagicProject/routes/elems.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Elem = require("../models/magic").Elem
+var async = require("async")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,19 +10,29 @@ router.get('/', function(req, res, next) {
 
 router.get("/:nick", async (req, res, next) => {
     try {
-      const elem = await Elem.findOne({ nick: req.params.nick });
-      console.log(elem);
+        const [elem, elems] = await Promise.all([
+            Elem.findOne({ nick: req.params.nick }),
+            Elem.find({}, { _id: 0, title: 1, nick: 1 })
+          ]);
       if (!elem) {
         throw new Error("Стихия испарилась");
       }
-      res.render('magic', {
-        title: elem.title,
-        picture: elem.avatar,
-        desc: elem.desc
-      });
+      renderMagic(res, elem.title, elem.avatar, elem.desc, elems);
     } catch (err) {
       next(err);
     }
   });
+
+  function renderMagic(res, title, picture, desc, elems) {
+    console.log(elems);
+  
+    res.render('magic', {
+        title: title,
+        picture: picture,
+        desc: desc,
+        menu:elems
+
+    });
+  }
 
 module.exports = router;
--- MagicProject/routes/index.js
+++ MagicProject/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var Elem = require("../models/magic").Elem
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Elem.find({},{_id:0,title:1,nick:1},function(err,menu){
+    res.render('index', {
+    title: 'Express',
+    menu: menu
+    });
+    })
 });
 
 
